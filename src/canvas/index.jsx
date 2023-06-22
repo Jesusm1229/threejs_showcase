38f6eaa576facb86d549f98261e255a903f830bc
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2023-06-22 11:44:54 -0400

    changes

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -6,24 +6,24 @@ import Backdrop from './Backdrop';
 import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
-    return (
-        <Canvas
-            shadows
-            camera={{ position: [0, 0, 0], fov: 25 }}
-            gl={{ preserveDrawingBuffer: true }}
-            className="w-full max-w-full h-full transition-all ease-in"
-        >
-            <ambientLight intensity={0.5} />
-            <Environment preset="city" />
+  return (
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
 
-            <CameraRig>
-                <Backdrop />
-                <Center>
-                    <Shirt />
-                </Center>
-            </CameraRig>
-        </Canvas>
-    )
+      <CameraRig>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  )
 }
 
 export default CanvasModel
